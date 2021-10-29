8a646ff28591e22dd42563bf12f6f4abb0496b29
Trond Vindenes <vindenesen@gmail.com>
2021-10-29 19:37:11 +0000

    Enabled forwarding of ipv4 packages in sysctl.conf

--- stage2/02-net-tweaks/01-run.sh
+++ stage2/02-net-tweaks/01-run.sh
@@ -34,3 +34,6 @@ else
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-3f300000.mmcnr:wlan"
     echo 1 > "${ROOTFS_DIR}/var/lib/systemd/rfkill/platform-fe300000.mmcnr:wlan"
 fi
+
+# Enable forwarding
+echo "net.ipv4.ip_forward = 1" > /etc/sysctl.conf
